afd91b2556bf6be1ebf6d9e15bb8c382bfa8563d
kkoryaka <31669748+kkoryaka@users.noreply.github.com>
2021-06-09 12:19:24 -0700

    added cannabis color scheme into oceanside theme

--- js/cagov.core.js
+++ js/cagov.core.js
@@ -1,5 +1,5 @@
 /**
- * CA State Template v5.5 -  @version v5.5.22 -  6/2/2021 
+ * CA State Template v5.5 -  @version v5.5.22 -  6/9/2021 
   STYLES COMPILED FROM SOURCE (source/js) DO NOT MODIFY */
 /*! modernizr (Custom Build) | MIT *
  * https://modernizr.com/download/?-flexbox-setclasses !*/
